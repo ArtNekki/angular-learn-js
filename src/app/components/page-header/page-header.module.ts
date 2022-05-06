cb41271db2b06c3d0ca9e29bea69517e1339cf81
Anton Mayatskiy <work.mayatskiy@gmail.com>
2022-05-06 14:06:04 +1000

    feat: top-banner done

--- src/app/components/page-header/page-header.module.ts
+++ src/app/components/page-header/page-header.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PageHeaderComponent } from './page-header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { TopBannerComponent } from './components/top-banner/top-banner.component';
 
 
 
 @NgModule({
-  declarations: [PageHeaderComponent, NavComponent],
+  declarations: [PageHeaderComponent, NavComponent, TopBannerComponent],
   imports: [
     CommonModule,
     SharedModule
