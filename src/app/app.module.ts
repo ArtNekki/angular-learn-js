7ee40c6871a8cb137273e4ac47a8ca5df80723b9
Anton Mayatskiy <work.mayatskiy@gmail.com>
2022-05-27 15:04:00 +1000

    feat: create interceptors and let directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageHeaderModule } from './components/page-header/page-header.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BaseUrlInterceptor } from './shared/interceptors/base-url.interceptor';
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule, PageHeaderModule],
-	providers: [],
+	providers: [
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: BaseUrlInterceptor,
+			multi: true,
+		},
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
