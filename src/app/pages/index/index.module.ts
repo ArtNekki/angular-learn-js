fa983df3a7d2e3c66b10630c26eae352a6a9ba67
Anton Mayatskiy <work.mayatskiy@gmail.com>
2022-05-30 06:41:19 +1000

    feat: add routing for index page

--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index.component';
+import { MainProductsModule } from './components/main-products/main-products.module';
+import { IndexRoutingModule } from './index-routing.module';
 
 @NgModule({
 	declarations: [IndexComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, MainProductsModule, IndexRoutingModule],
 })
 export class IndexModule {}
