fdd59e69c3db9ad3a5f3e866af7ed713629be22a
Anton Mayatskiy <work.mayatskiy@gmail.com>
2022-05-30 17:43:59 +1000

    feat: add markup

--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index.component';
 import { MainProductsModule } from './components/main-products/main-products.module';
 import { IndexRoutingModule } from './index-routing.module';
+import { IconModule } from 'src/app/components/icon/icon.module';
 
 @NgModule({
 	declarations: [IndexComponent],
-	imports: [CommonModule, MainProductsModule, IndexRoutingModule],
+	imports: [CommonModule, MainProductsModule, IndexRoutingModule, IconModule],
 })
 export class IndexModule {}
